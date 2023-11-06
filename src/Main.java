66e668964d0d9668026fb0853c43f55ab48ea7c7
e146508 <Dileep.Kessireddy@wnco.com>
2023-11-06 13:13:16 -0600

    Rename Matt with Joe

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 
-//        SalariedEmployee emp = new SalariedEmployee("Matt", "12-21-1990",
-//                "12-31-2024", 146508, "10-04-2021", 100000, true);
-
-
-
+        
         Employee tim = new Employee("Tim", "11/11/1985", "01/01/2020");
 
         System.out.println(tim);
@@ -15,14 +11,14 @@ public class Main {
         System.out.println("Age ="+ tim.getAge());
         System.out.println("Pay ="+ tim.collectPay());
 
-        SalariedEmployee matt = new SalariedEmployee("Matt", "11/11/1989", "01/01/2021", 100000);
+        SalariedEmployee joe = new SalariedEmployee("joe", "11/11/1989", "01/01/2021", 100000);
 
-        System.out.println(matt);
-        System.out.println("Matt's Paycheck =$"+ matt.collectPay());
+        System.out.println(joe);
+        System.out.println("joe's Paycheck =$"+ joe.collectPay());
 
-        matt.retire();
+        joe.retire();
 
-        System.out.println("Matt's Pension check = $" + matt.collectPay());
+        System.out.println("joe's Pension check = $" + joe.collectPay());
 
         HourlyEmployee mary = new HourlyEmployee("Mary", "12/21/1990",
                 "10/01/2021", 15);
